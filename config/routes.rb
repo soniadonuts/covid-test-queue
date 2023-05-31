8ae91c3c461d4ea8018a836140fd7394cb7d99bc
soniadonuts <soniag@jobcan.in.th>
2023-06-01 00:07:29 +0700

    FINISH function1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "home#index"
 end
---
+++ db/migrate/20230531150339_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :surname, null: false
+      t.string :gender, null: false
+      t.date :dob, null: false
+      t.string :citizen_id, null: false
+      t.string :address, null: false
+      t.string :contact_number, null: false
+      t.integer :user_role, null: false, default: 0
+      t.string :password_digest, null: false
+
+      t.timestamps
+    end
+  end
+end
